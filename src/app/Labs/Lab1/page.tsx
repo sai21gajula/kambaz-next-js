f74f2acfaecff7325f19a6e6686ac2e63d249123
bharathg <bharath@example.com>
2025-09-22 21:05:26 -0500

    testing  the signin page deployment

--- src/app/Labs/Lab1/page.tsx
+++ src/app/Labs/Lab1/page.tsx
@@ -5,6 +5,12 @@ export default function Lab1() {
   return (
     <div id="wd-lab1">
       <h2>Lab 1</h2>
+      <div >
+        <strong>Name:</strong> Bharath Gajula<br />
+        <strong>Course:</strong> CS5610 Web Development, Fall 2025 <br/>
+        <strong>Section:</strong> 18616.202610<br />
+        <strong>GitHub:</strong> <a href="https://github.com/sai21gajula/kambaz-next-js" target="_blank" rel="noopener noreferrer">Kambaz G</a>
+      </div>
         <h3>HTML Examples</h3>
       <div id="wd-h-tag">
         <h4>Heading Tags</h4>

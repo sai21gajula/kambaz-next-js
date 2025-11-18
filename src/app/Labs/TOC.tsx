216430a98802059918adb31627486c36c1a2053b
Bharath Gajula <163699778+sai21gajula@users.noreply.github.com>
2025-11-17 23:21:30 -0600

    updated toc

--- src/app/Labs/TOC.tsx
+++ src/app/Labs/TOC.tsx
@@ -34,32 +34,9 @@ export default function TOC() {
        <NavLink href="/" as={Link}> Kambaz </NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/jannunzi">My GitHub</NavLink></NavItem>
+       <NavLink href="https://github.com/sai21gajula/kambaz-next-js">My  Kanbaz GitHub</NavLink></NavItem>
+    <NavItem>
+      <NavLink href="https://github.com/sai21gajula/-kambaz-node-server-app" target="_blank" rel="noopener noreferrer">Node Server Backend (GitHub)</NavLink>
+    </NavItem>
    </Nav>
  );}
-
-// import Link from "next/link";
-// export default function TOC() {
-//  return (
-//    <ul>
-//      <li>
-//        <Link href="/Labs" id="wd-lab1-link">
-//          Home </Link>
-//      </li>
-//      <li>
-//        <Link href="/Labs/Lab1" id="wd-lab1-link">
-//          Lab 1 </Link>
-//      </li>
-//      <li>
-//        <Link href="/Labs/Lab2" id="wd-lab2-link">
-//          Lab 2 </Link>
-//      </li>
-//      <li>
-//        <Link href="/Labs/Lab3" id="wd-lab3-link">
-//          Lab 3 </Link>
-//      </li>
-//       <li>
-//        <Link href="/" id="wd-kambaz-link">
-//          Kambaz </Link> </li>
-//    </ul>
-// );}
